6f547bcaae720bbec06f5e58430a64bd9b811a0d
Imran940 <65508069+Imran940@users.noreply.github.com>
2021-04-19 12:18:21 +0530

    updating with knex

--- middlewares/verifyTrans.js
+++ middlewares/verifyTrans.js
@@ -1,11 +1,5 @@
-const mysql = require("mysql");
+const db = require('../db/database');
 const jwt = require("jsonwebtoken");
-const db = mysql.createConnection({
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-})
 exports.verifyToken = (req, res, next) => {
     const token = req.headers.token;
     jwt.verify(token, 'password', (err, resp) => {
@@ -16,24 +10,20 @@ exports.verifyToken = (req, res, next) => {
         }
     })
 }
-
-
-exports.getBalance = (req, res, next) => {
+exports.getBalance = async (req, res, next) => {
     let balance;
     const username = req.headers.username;
-    const balSql = "select balance from account where CustName=?";
-    db.query(balSql, [username], (err, result) => {
-        if (result.length == 0) {
-            balance = 0;
-            req.balance = balance;
-            next();
-        } else if (!err) {
-            balance = parseInt(result[result.length - 1].balance)
-            req.balance = balance;
-            next();
-        } else {
-            console.log(err);
-        }
-    })
+    const result = await db('account').where({
+        CustName: username
+    }).select('balance')
 
+    if (result.length === 0) {
+        balance = 0;
+        req.balance = balance;
+        next();
+    } else {
+        balance = parseInt(result[result.length - 1].balance)
+        req.balance = balance;
+        next();
+    }
 }
