92d87b120ef8b71182d20e26b1ce06bcd9dd7642
Imran940 <65508069+Imran940@users.noreply.github.com>
2021-04-19 12:16:23 +0530

    updaing with knex

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,22 @@
-const mysql = require("mysql");
+const db = require('../db/database')
 const jwt = require("jsonwebtoken");
-const db = mysql.createConnection({
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-})
-exports.checkUser = (req, res) => {
+exports.checkUser = async (req, res) => {
     const { username, password } = req.body;
-    db.query('select * from login where username=? and password=?', [username, password], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(result);
-            jwt.sign({ user: result[0] }, 'password', (error, token) => {
-                if (!err) {
-                    res.send(token);
-                } else {
-                    console.log(error)
-                }
+    const result = await db('login').where({
+        username: username,
+        password: password
+    }).select()
 
-            })
+    if (result.length > 0) {
+        jwt.sign({ user: result[0] }, 'password', (error, token) => {
+            if (!error) {
+                res.send(token);
+            } else {
+                console.log(error)
+            }
 
-        }
-    })
+        })
+    }
+
 
 }
